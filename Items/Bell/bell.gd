6d763e76400c0e0d646a9eb66d6bbf3306f0f4d7
Andrew Liu <andrewliu101720@gmail.com>
2025-05-12 22:20:00 -0600

    bell and ofuda HUD added

--- Items/Bell/bell.gd
+++ Items/Bell/bell.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
+@export var display_item: bool = false
+
 @onready var bell_rang_label: Label = $"Bell Rang"
 
 func _ready() -> void:
 	SignalBus.bell_rang.connect(_on_bell_rang)
 
 func _on_bell_rang(volume: float):
+	if display_item:
+		return
 	bell_rang_label.visible = true
 	await get_tree().create_timer(1).timeout
 	bell_rang_label.visible = false
--- Items/Ofuda/ofuda.gd
+++ Items/Ofuda/ofuda.gd
@@ -1,16 +1,26 @@
 extends Node2D
 
+@export var display_item: bool = false
 @export var ofuda_placed: bool = false
 
 @onready var ofuda_placed_label: Label = $"Ofuda Placed"
 
+var ofuda_count_lable: Label
 var ofuda_count: int = 0
 
 func _ready() -> void:
 	SignalBus.ofuda_placed.connect(_on_ofuda_place)
+	if display_item:
+		ofuda_count_lable = $Labels/Value
 
 func _on_ofuda_place(count: int):
 	ofuda_count = count
+	if ofuda_count_lable != null:
+		ofuda_count_lable.text = str(2-ofuda_count)
+	if ofuda_count_lable != null and ofuda_count == 2:
+		ofuda_count_lable.add_theme_color_override("font_color", Color.RED) 
+	if display_item:
+		return
 	ofuda_placed_label.visible = true
 	await get_tree().create_timer(1).timeout
 	ofuda_placed_label.visible = false
