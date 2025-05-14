bc23a16439915e4eea2a76aafc38f441e67f383c
Andrew Liu <andrewliu101720@gmail.com>
2025-05-14 14:05:40 -0600

    implemented animations for other ghost types

--- Globals/Global.gd
+++ Globals/Global.gd
@@ -3,16 +3,16 @@ extends Node
 @onready var player_node: Node2D
 @onready var ofuda: PackedScene = preload("res://Items/Ofuda/ofuda.tscn")
 @onready var yuki_onna: PackedScene = preload("res://Ghosts/Yuki-Onna/yuki_onna.tscn")
+@onready var onryo: PackedScene = preload("res://Ghosts/Onryo/onryo.tscn")
+@onready var jikininki: PackedScene = preload("res://Ghosts/Jikininki/jikininki.tscn")
 
 @onready var ghost_scene_array: Array[PackedScene] = [
 	yuki_onna,
-	
+	onryo,
+	jikininki
 ]
 
 var ghost_detected: bool = false
 var ghost_direction: Vector2
 var player_position: Vector2
 var ghost_position: Vector2
-
-func vec_len(vector: Vector2) -> float:
-	return sqrt((vector.x ** 2) + (vector.y ** 2))
