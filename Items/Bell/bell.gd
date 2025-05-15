e9cf63218544e6fae474cd5213d42f1100d375b4
Andrew Liu <andrewliu101720@gmail.com>
2025-05-14 20:53:26 -0600

    code cleanup and refactor
    - Node_tree same level node references are more reliable (slightly more decoupled)
    - Error messages and stop game for missing components
    - Added ghost detection component
    - Refactored player ghost detection system
    - Added ghost detection system for ofuda and bell items
    - Refactored global script
    - Code cleanup

--- Items/Bell/bell.gd
+++ Items/Bell/bell.gd
@@ -1,15 +1,42 @@
 extends Node2D
 
+@export var ghost_detection_component: GhostDetectionComponent
 @export var display_item: bool = false
 
 @onready var bell_rang_label: Label = $"Bell Rang"
 
+var bell_rang: bool = false
+
 func _ready() -> void:
+	if not ghost_detection_component:
+		assert(false, "Ghost Detection Component not found.")
+	
 	SignalBus.bell_rang.connect(_on_bell_rang)
+	if display_item:
+		ghost_detection_component.process_mode = Node.PROCESS_MODE_DISABLED
+		ghost_detection_component.collision.visible = false
+
+func _process(delta: float) -> void:
+	if not display_item:
+		_update_item_response()
 
 func _on_bell_rang(_volume: float):
 	if display_item:
 		return
 	bell_rang_label.visible = true
+	bell_rang = true
 	await get_tree().create_timer(1).timeout
 	bell_rang_label.visible = false
+	bell_rang = false
+
+func _update_item_response() -> void:
+	if ghost_detection_component.ghost_detected and bell_rang and ghost_detection_component.ghost_type == Global.yuki_onna_node:
+		print("Yuki Detected")
+	elif ghost_detection_component.ghost_detected and bell_rang and ghost_detection_component.ghost_type == Global.onryo_node:
+		print("Onryo Detected")
+	elif ghost_detection_component.ghost_detected and bell_rang and ghost_detection_component.ghost_type == Global.jikininki_node:
+		print("Jikininki Detected")
+	elif ghost_detection_component.ghost_detected and not bell_rang:
+		print("Not ringing")
+	elif not ghost_detection_component.ghost_detected:
+		print("Ghost not detected")
--- Maps/Map 1/map_1.gd
+++ Maps/Map 1/map_1.gd
@@ -2,13 +2,23 @@ class_name Map
 extends Node2D
 
 @export var spawner_component: SpawnerComponent
-
-@onready var book_ui = $CanvasLayer
+@export var world_objects_node: Node2D
+@export var book_ui: CanvasLayer
 
 func _ready() -> void:
+	Global.world_node = self
+	if world_objects_node:
+		Global.world_objects_node = world_objects_node
+	else:
+		world_objects_node = Node2D.new().instantiate()
+		world_objects_node.y_sort_enabled = true
+		Global.world_objects_node = world_objects_node
+		add_child(world_objects_node)
 	spawner_component.point_spawn()
 
-func _process(_delta):
-	if Input.is_action_just_pressed("toggle_journal"):
-		book_ui.visible = !book_ui.visiblawe
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE if book_ui.visible else Input.MOUSE_MODE_CAPTURED)
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("toggle_journal") and book_ui:
+		book_ui.visible = !book_ui.visible
+		#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE if book_ui.visible else Input.MOUSE_MODE_CAPTURED)
+	elif Input.is_action_just_pressed("toggle_journal") and not book_ui:
+		assert(false, "Book UI CanvasLayer not found in root node.")
