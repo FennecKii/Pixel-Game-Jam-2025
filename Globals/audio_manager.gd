0c8ff7bb3dbd3ee295501d89212adb0a9b1529eb
Andrew Liu <andrewliu101720@gmail.com>
2025-05-18 11:59:39 -0600

    audio manager changes

--- Globals/audio_manager.gd
+++ Globals/audio_manager.gd
@@ -17,17 +17,16 @@ func _ready() -> void:
 func play_music_background(type: MusicResource.MusicType) -> void:
 	if main_background_music.stream == music_track_dict[type].sound:
 		return
-	else:
+	elif music_track_dict.has(type):
 		var music_track: MusicResource = music_track_dict[type]
 		main_background_music.bus = "Music"
 		main_background_music.stream = music_track.sound
-		main_background_music.stream.loop_mode = AudioStreamWAV.LoopMode.LOOP_FORWARD
-		main_background_music.stream.loop_begin = 0
-		main_background_music.stream.loop_end = int(10*float(music_track.sound.mix_rate))
 		main_background_music.volume_db = music_track.volume
 		main_background_music.pitch_scale = music_track.pitch_scale
 		main_background_music.pitch_scale += Global.rng.randf_range(-music_track.pitch_randomness, music_track.pitch_randomness )
 		main_background_music.play()
+	else:
+		push_error("Audio Manager failed to find type ", type)
 
 func play_music(type: MusicResource.MusicType) -> void:
 	if music_track_dict.has(type):
@@ -35,7 +34,6 @@ func play_music(type: MusicResource.MusicType) -> void:
 		var new_audio: AudioStreamPlayer = AudioStreamPlayer.new()
 		new_audio.bus = "Music"
 		new_audio.stream = music_track.sound
-		new_audio.stream.set_loop_mode(AudioStreamWAV.LoopMode.LOOP_FORWARD)
 		new_audio.volume_db = music_track.volume
 		new_audio.pitch_scale = music_track.pitch_scale
 		new_audio.pitch_scale += Global.rng.randf_range(-music_track.pitch_randomness, music_track.pitch_randomness )
@@ -44,18 +42,25 @@ func play_music(type: MusicResource.MusicType) -> void:
 	else:
 		push_error("Audio Manager failed to find type ", type)
 
-#TODO
-func music_loop():
-	pass
-
-func music_fade():
-	pass
-
-func sfx_loop():
-	pass
-
-func sfx_repeat():
-	pass
+func play_music_loop(type: MusicResource.MusicType, loop_begin: float = 0, loop_end: float = -1) -> void:
+	if music_track_dict.has(type):
+		var music_track: MusicResource = music_track_dict[type]
+		var new_audio: AudioStreamPlayer = AudioStreamPlayer.new()
+		new_audio.bus = "Music"
+		new_audio.stream = music_track.sound
+		new_audio.stream.loop_mode = AudioStreamWAV.LoopMode.LOOP_FORWARD
+		new_audio.stream.loop_begin = loop_begin * music_track.sound.mix_rate
+		if loop_end == -1:
+			new_audio.stream.loop_end = music_track.sound.get_length() * music_track.sound.mix_rate
+		else:
+			new_audio.stream.loop_end = loop_end * music_track.sound.mix_rate
+		new_audio.volume_db = music_track.volume
+		new_audio.pitch_scale = music_track.pitch_scale
+		new_audio.pitch_scale += Global.rng.randf_range(-music_track.pitch_randomness, music_track.pitch_randomness )
+		add_child(new_audio)
+		new_audio.play()
+	else:
+		push_error("Audio Manager failed to find type ", type)
 
 func play_sfx_at_location(location: Vector2, type: SoundResource.SoundType) -> void:
 	if sound_effect_dict.has(type):
--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-func _ready() -> void:
-	AudioManager.play_music_background(MusicResource.MusicType.BACKGROUND_AMBIENCE_TRACK_1)
-
 func _on_pressed_play():
 	get_tree().change_scene_to_file("res://UI/map_select.tscn")
 
