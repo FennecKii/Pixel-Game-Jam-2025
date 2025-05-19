ed2415859bf58c921eacdc47b3c6730bca9b14d7
Andrew Liu <andrewliu101720@gmail.com>
2025-05-19 16:22:54 -0600

    small ui changes

--- UI/win_screen.gd
+++ UI/win_screen.gd
@@ -7,7 +7,6 @@ func _on_play_again_pressed():
 	AudioManager.play_sfx_global(SoundResource.SoundType.BUTTON_PRESS)
 	get_tree().change_scene_to_file("res://UI/map_select.tscn")
 
-
 func _on_main_menu_pressed():
 	AudioManager.play_sfx_global(SoundResource.SoundType.BUTTON_PRESS)
 	get_tree().change_scene_to_file("res://UI/main_menu.tscn")
