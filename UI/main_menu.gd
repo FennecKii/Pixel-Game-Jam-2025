39919849a7fe668606cf608e7239c71b0b6069b0
Andrew Liu <andrewliu101720@gmail.com>
2025-05-19 00:59:47 -0600

    main menu music

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,5 +1,18 @@
 extends Node2D
 
+var ambient_audio_played: bool = false
+
+func _ready() -> void:
+	AudioManager.play_background_track2(MusicResource.MusicType.BACKGROUND_AMBIENCE_LOOP)
+
+func _process(delta: float) -> void:
+	if not ambient_audio_played:
+		if randf_range(0, 1) <= 0.025:
+			ambient_audio_played = true
+			AudioManager.play_sfx_global(randi_range(SoundResource.SoundType.AMBIENT_KNOCK_1, SoundResource.SoundType.AMBIENT_WHISPER))
+			await get_tree().create_timer(10).timeout
+			ambient_audio_played = false
+
 func _on_pressed_play():
 	get_tree().change_scene_to_file("res://UI/map_select.tscn")
 
