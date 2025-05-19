b9d9beb22273b26d9ba325dcc5829750ceab5dfe
imbretty <114277284+imbrettirl@users.noreply.github.com>
2025-05-19 14:45:27 -0600

    Map Buttons

--- UI/map_select.gd
+++ UI/map_select.gd
@@ -4,4 +4,4 @@ func _on_map1_pressed():
 	get_tree().change_scene_to_file("res://Maps/Map 1/map_1.tscn")
 
 func _on_map_2_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Maps/Map2/map2.tscn")
