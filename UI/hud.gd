a435e8af84a99a73ade50e4feebf4ea700fa89e9
Andrew Liu <83555681+FennecKii@users.noreply.github.com>
2025-05-19 20:35:42 -0600

    item and mirror conditions added (#39)
    - Conditions are on random chance
    - Made ghost detection on items bigger
    - Fixed bell cooldown label
    - Mirror crack stays cracked
    - Fixed some audio mismatches

--- UI/hud.gd
+++ UI/hud.gd
@@ -1,3 +1,7 @@
 extends CanvasLayer
 
 @onready var compass: Node2D = %Compass
+@onready var bell_cooldown_label: Label = $Control/Items/Bell/Labels/Value
+
+func _ready() -> void:
+	Global.bell_cooldown_label = bell_cooldown_label
