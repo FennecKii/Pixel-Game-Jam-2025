41d1fda6b3b0838f4dc2ca80537d86f4419ff105
Andrew Liu <andrewliu101720@gmail.com>
2025-05-19 21:08:58 -0600

    small win screen changes

--- UI/win_screen.gd
+++ UI/win_screen.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 func _ready():
 	$FadeRect/AnimationPlayer.play("fade_in")
-	AudioManager.clear_audio()
 	AudioManager.stop_background_track1()
 	AudioManager.stop_background_track2()
-	#AudioManager.play_background_track2(MusicResource.MusicType.BACKGROUND_AMBIENCE_LOOP)
+	AudioManager.clear_audio()
+	AudioManager.play_background_track2(MusicResource.MusicType.BACKGROUND_AMBIENCE_LOOP)
 
 func _on_play_again_pressed():
 	AudioManager.play_sfx_global(SoundResource.SoundType.BUTTON_PRESS)
