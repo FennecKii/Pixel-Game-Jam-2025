b9d93ac8357254ccbd478c3bdf0620fbf413ccd2
Andrew Liu <andrewliu101720@gmail.com>
2025-05-19 20:49:51 -0600

    instruction scene keybind set

--- UI/instructions.gd
+++ UI/instructions.gd
@@ -4,9 +4,14 @@ extends Control
 @onready var page2 = $Page2
 
 func _ready():
-	AudioManager.play_sfx_global(SoundResource.SoundType.JOURNAL_PAGE_FLIP)
+	AudioManager.play_sfx_global(SoundResource.SoundType.JOURNAL_OPEN)
 	page1.visible = true
 	page2.visible = false
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Instructions"):
+		AudioManager.play_sfx_global(SoundResource.SoundType.JOURNAL_OPEN)
+		visible = true
 
 func _on_texture_button_pressed():
 	AudioManager.play_sfx_global(SoundResource.SoundType.JOURNAL_PAGE_FLIP)
@@ -19,4 +24,5 @@ func _on_back_button_pressed():
 	page2.visible = false
 
 func _on_exit_button_pressed():
-	queue_free()
+	AudioManager.play_sfx_global(SoundResource.SoundType.JOURNAL_CLOSE)
+	visible = false
